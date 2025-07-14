4942981f280b6bdd1ba6c8e02b4052aa248f281c
Roque Alberto Pulido Morales <rapulidom@desarrollo-ultrasist.com.mx>
2025-07-13 21:51:34 -0600

    CHANGE BaseUrl build

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-   output: 'export'
+  basePath: '/wdd430-group-project',
+  assetPrefix: '/wdd430-group-project/',
+  output: 'export'
 };
 
 export default nextConfig;
