fe67d704dbb67353b7b5f86161e873d96c831b88
Roque Alberto Pulido Morales <rapulidom@desarrollo-ultrasist.com.mx>
2025-07-13 22:45:57 -0600

    add img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src="./next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
@@ -60,7 +60,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/file.svg"
+            src="./file.svg"
             alt="File icon"
             width={16}
             height={16}
@@ -75,7 +75,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/window.svg"
+            src="./window.svg"
             alt="Window icon"
             width={16}
             height={16}
@@ -90,7 +90,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/globe.svg"
+            src="./globe.svg"
             alt="Globe icon"
             width={16}
             height={16}
